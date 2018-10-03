9257bb08bff88d9ebd47c5015760359b4e096b3b
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-10-04 00:41:42 +0200

    correction

--- examples/raxml.R
+++ examples/raxml.R
@@ -1,3 +1,4 @@
 library(outsider)
-raxml <- module_import('raxml', repo = 'dombennett/om..raxml..8.2.12')
+raxml <- module_import('raxml',
+                       repo = 'dombennett/om..raxml..8.2.12.pthreads.sse3')
 raxml('-h')
