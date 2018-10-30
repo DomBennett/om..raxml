4faf5488dd28427854d59244ba2e3643f69d1913
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-10-30 16:12:26 +0100

    updates

--- R/functions.R
+++ R/functions.R
@@ -7,8 +7,9 @@ pkgnm <- environmentName(env = environment())
 #' @example examples/raxml.R
 #' @export
 raxml <- function(...) {
-  args <- outsider::.args_parse(...)
+  args <- outsider::.args_parse()
   files_to_send <- outsider::.which_args_are_filepaths(args)
+  args <- outsider::.to_basename(args)
   outsider::.run(pkgnm = pkgnm, files_to_send = files_to_send,
-                 'raxmlHPC-PTHREADS-SSE3', args)
+                 cmd = 'raxmlHPC-PTHREADS-SSE3', args = args)
 }
