6415ad1e1752d52603eab290b7c6209406306dc5
Dom Bennett <dominic.john.bennett@gmail.com>
2019-09-04 15:17:53 +0200

    outdir created

--- R/functions.R
+++ R/functions.R
@@ -4,13 +4,14 @@ pkgnm <- environmentName(env = environment())
 #' @title raxml
 #' @description Run raxml
 #' @param arglist Arguments to raxml provided as a character vector
+#' @param outdir Filepath to where all output files should be returned.
 #' @example examples/raxml.R
 #' @export
-raxml <- function(arglist = arglist_get(...)) {
+raxml <- function(arglist = arglist_get(...), outdir = getwd()) {
   files_to_send <- filestosend_get(arglist)
   arglist <- arglist_parse(arglist)
   otsdr <- outsider_init(pkgnm = 'om..raxml', cmd = 'raxmlHPC-PTHREADS-SSE3',
-                         wd = getwd(), files_to_send = files_to_send,
+                         wd = outdir, files_to_send = files_to_send,
                          arglist = arglist)
   run(otsdr)
 }
