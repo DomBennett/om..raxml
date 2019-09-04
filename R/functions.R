14beef6358797cc286e0eb15031c44c45d93c221
Dom Bennett <dominic.john.bennett@gmail.com>
2019-09-04 14:43:18 +0200

    switching to arglist

--- R/functions.R
+++ R/functions.R
@@ -3,11 +3,10 @@ pkgnm <- environmentName(env = environment())
 #' @name raxml
 #' @title raxml
 #' @description Run raxml
-#' @param ... Arguments
+#' @param arglist Arguments to raxml provided as a character vector
 #' @example examples/raxml.R
 #' @export
-raxml <- function(...) {
-  arglist <- arglist_get(...)
+raxml <- function(arglist = arglist_get(...)) {
   files_to_send <- filestosend_get(arglist)
   arglist <- arglist_parse(arglist)
   otsdr <- outsider_init(pkgnm = 'om..raxml', cmd = 'raxmlHPC-PTHREADS-SSE3',
